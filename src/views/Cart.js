2b2c172fc4d1967062b3dafdebd5699e23c2be08
Adrian <adrian.w.lub@gmail.com>
2024-04-18 19:59:49 +0200

    Info about reservation that done or no

--- src/views/Cart.js
+++ src/views/Cart.js
@@ -4,49 +4,58 @@ import { cartManager } from "../cart/cart-manager";
 export function Cart() {
   const section = document.createElement('section');
 
-  section.innerHTML = `
-    <h2>Zamówienie</h2>
-    <p>Przegląd zawartosci swoich rezerwacji.</p>
-    <table class="table">
-        <tr>
-         <th>Nazwa</th>
-         <th>Liczba</th>
-         <th>Cena</th>
-         <th></th>
-        </tr>
-    </table>
-    `;
-  const tableRows = cartManager.getAll().map((item) => {
-    const tr = document.createElement('tr');
+  let cartItems = cartManager.getAll();
 
-    tr.innerHTML = `
-        <td>${item.name}</td>
-        <td>${item.quality}</td>
-        <td>${(item.quality * item.price).toFixed(2)}</td>
+  if (cartItems.length === 0) {
+    section.innerHTML = `
+      <p class="empty-cart-message">Brak dokonanych rezerwacji</p>
+    `;
+  } else {
+    section.innerHTML = `
+      <h2>Zamówienie</h2>
+      <p>Przegląd zawartości swoich rezerwacji.</p>
+      <table class="table">
+          <tr>
+           <th>Nazwa</th>
+           <th>Liczba</th>
+           <th>Cena</th>
+           <th></th>
+          </tr>
+      </table>
+    `;
+
+    const tableRows = cartManager.getAll().map((item) => {
+      const tr = document.createElement('tr');
+
+      tr.innerHTML = `
+          <td>${item.name}</td>
+          <td>${item.quality}</td>
+          <td>${(item.quality * item.price).toFixed(2)} PLN</td>
+          <td></td>
+      `;
+
+      const removeFromCartButton = RemoveFromCartButton(() =>
+        cartManager.remove(item)
+      );
+
+      tr.lastElementChild.append(removeFromCartButton);
+
+      return tr;
+    });
+
+    const tableFooter = document.createElement('tr');
+
+    tableFooter.innerHTML = `
+        <td></td>
+        <td></td>
+        <td>
+            <strong>${cartManager.getTotalPrice()} PLN</strong>
+        </td>
         <td></td>
     `;
-    const removeFromCartButton = RemoveFromCartButton(() =>
-      cartManager.remove(item)
-    
-    );
 
-    tr.lastElementChild.append(removeFromCartButton);
-
-    return tr;
-  });
-
-  const tableFooter = document.createElement('tr');
-
-  tableFooter.innerHTML = `
-      <td></td>
-      <td></td>
-      <td>
-          <strong>${cartManager.getTotalPrice()} PLN</strong>
-      </td>
-      <td></td>
-  `;
-
-  section.querySelector('table').append(...tableRows, tableFooter);
+    section.querySelector('table').append(...tableRows, tableFooter);
+  }
 
   return section;
 }
