2da36271ffc0d70f98f8c61441472b775f808e5d
Adrian <adrian.w.lub@gmail.com>
2024-04-26 13:52:04 +0200

    Add burger menu work at responsive web

--- src/navigation/Nav.js
+++ src/navigation/Nav.js
@@ -2,14 +2,23 @@ import { Cart } from '../views/Cart';
 import { Home } from '../views/Home';
 import { RoomList } from '../views/RoomList';
 import { TreatmentList } from '../views/TreatmentList';
-import { Registration } from '../common/registration';
+import { Registration } from '../common/Registration';
 import { NavButton } from './NavButton';
-import { Login } from '../common/login';
+import { Login } from '../common/Login';
+
+// Importowanie ikony FontAwesome
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { dom } from '@fortawesome/fontawesome-svg-core';
+
+// Dodanie ikony do biblioteki FontAwesome
+library.add(faBars);
+dom.watch();
 
 const navItems = [
-  { text: "Stronga Główna", component: Home },
+  { text: "Strona Główna", component: Home },
   { text: "Zobacz Pokoje", component: RoomList },
-  { text: 'Zabiegi', component: TreatmentList },
+  { text: "Zabiegi", component: TreatmentList },
   { text: "Rezerwacja", component: Cart },
   { text: "Rejestracja", component: Registration },
   { text: "Logowanie", component: Login },
@@ -17,12 +26,52 @@ const navItems = [
 
 export function Nav() {
   const nav = document.createElement("nav");
+  nav.classList.add("nav");
 
-  const navButton = navItems.map((navItem) => {
-    return NavButton(navItem.text, navItem.component);
+  // Tworzenie diva dla hamburgera
+  const hamburgerDiv = document.createElement("div");
+  hamburgerDiv.classList.add("hamburger");
+
+  const icon = document.createElement("i");
+  icon.classList.add("fas", "fa-bars"); // Ikona z FontAwesome
+
+  // Dodanie ikony do diva hamburgera
+  hamburgerDiv.appendChild(icon);
+
+  // Tworzenie aside z listą nawigacyjną
+  const asideElement = document.createElement("aside");
+  asideElement.classList.add("asideElement");
+
+  const divElement = document.createElement("div");
+  divElement.classList.add("nav_list");
+
+  const navButtonElementsAside = navItems.map((navItem) => {
+    const button = NavButton(navItem.text, navItem.component);
+    button.classList.add("btn", "btn-primary");
+
+    button.addEventListener('click', function () {
+      if (window.innerWidth < 768) {
+        asideElement.style.display = 'none';
+      }
+    });
+
+    return button;
   });
 
-  nav.append(...navButton);
+  divElement.append(...navButtonElementsAside);
+  asideElement.appendChild(divElement);
+  nav.appendChild(hamburgerDiv);
+  nav.appendChild(asideElement);
+
+  hamburgerDiv.addEventListener('click', function () {
+    if (asideElement.style.display === 'none') {
+      asideElement.style.display = 'flex';
+    } else {
+      asideElement.style.display = 'none';
+    }
+  });
 
   return nav;
 }
+
+
